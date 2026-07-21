21c335c4c21b6b79908c1e8d943b0897878a6dcd
kent035 <davidasundaciv@gmail.com>
2026-07-21 12:01:51 +0300

    python basics

---
+++ operators.py
@@ -0,0 +1,49 @@
+#operators are used to perform operation on variables
+#arithmetic operators -perform mathematical operations
+#+,-,*,/,%
+#addition
+x=10
+y=35
+print("The sum is",sum)
+#subtraction -
+print(f"The difference is {y-x}")
+print("The difference is",y-x)
+#* multiplication
+print(f"the product{x} and {y} is {x*y}")
+#/-Division
+print(f"The division of{y} and {x} is {y/x}")
+#modulus %-the remainder of division
+print(f"The modulus of {y} and {x} is {y%x}")
+#**exponent
+print(x**3)
+#comparison operators.>,<,>=,<=,==,!=
+#greater than >
+print(f"Is {x} greater {y}? {x>y}")
+#less than <
+print(f"Is {x} less than {y}? {x<y}")
+#equality ==
+print(x==y)
+#greater than or equal to >=
+print(x>=y)
+#less than or equal to
+print(x<=y)
+#not equal to !=
+print(x!=y)
+#logical operators-AND,OR and NOT
+#AND-returns true if both statements are true
+z=5
+print(z>3 and z<10)
+print(z<3 and z<10)
+#or-returns true if one of the statements is true
+print(z<3 or z<10)
+#not-returns the reverse
+print(not(z<3))
+#Assignment operators-=,+=,-=,*=,/=
+a=20
+print(a)
+#+=
+a+=5# a=a+5
+print(a)
+#-=
+a-=2 #a=a-2
+print(a)
---
+++ strings.py
@@ -0,0 +1,44 @@
+#string- a sequence of characters./
+first_name="mark"
+last_name="john"
+print("my first name is",first_name)
+#+ concatenation
+fullname=first_name +  " "+last_name
+print(fullname)
+#f-string f""
+age=19
+print(f"my name is {fullname}  and i am {age} years old")
+#string methods upper(),lower() find(),replace()
+message="hello world"
+print(message.upper())
+#capitalize
+print(message.capitalize())
+#lower()
+print(message.lower())
+#find()
+print(message.find("e")) #returns the index
+print(message.replace("world","goodmorning"))
+#slicing/substring [start:stop]
+course="python"
+print(course)
+print(course[0:3])# from 0 to 2
+print(course[1:])# 1-end
+print(course[:4])#0-3
+print(course[::2])
+#text="fullstack"
+text="fullstack"
+print(text[0:4])# from 0 to 4
+print(text[4:9])
+print(text[0:6])
+#escape characters \n-newline,\t-tab
+print("hello this\n is python")
+#\t-tab-adds a tab space
+print("name\t:age")
+#'-adds a single quote
+print('it\`s a nice day')
+#\"-adds a double quote
+print("she said \"hello\"")
+#she said "i love python programming"
+print("python is easy \"mark john\"")
+#"python is easy"-mark john
+      
